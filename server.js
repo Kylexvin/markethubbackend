0700c33e592eb852d3fcb0050f3643a122a40a1c
Vinny Kylex <vinnykylex@gmail.com>
2024-11-11 12:31:30 +0300

    nov 11

--- server.js
+++ server.js
@@ -432,5 +432,5 @@ app.put('/api/admin/users/:userId/grant-admin', async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
